9b7e53dfd0106e35d593c148f5d51cabe97f105d
Kassidy Douglas <kcmdouglas@gmail.com>
2016-03-08 11:43:23 -0800

    Add function to set markers

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   map: Ember.inject.service('google-map'),
-
+  places: [["Cassidy's Restaurant", 45.522523, -122.685156], ["Lan Su Chinese Garden", 45.525464, -122.672964]],
   actions: {
     showMap(resultsMap) {
       var address = this.get('address');
@@ -14,6 +14,13 @@ export default Ember.Component.extend({
         //center: {lat: -34.397, lng: 150.644}
       };
       map.findAddress(container, options, address);
+      //setMarker(45.522523, -122.685156);
+      },
+
+      populatePlaces() {
+        var address = this.get('google-map.results.geometry.location');
+        var places = this.get('places');
       }
+
     }
 });
--- app/services/google-map.js
+++ app/services/google-map.js
@@ -5,7 +5,9 @@ export default Ember.Service.extend({
   lat: '',
   lng: '',
   results: '',
-
+  map: '',
+  places: [["Cassidy's Restaurant", 45.522523, -122.685156], ["Lan Su Chinese Garden", 45.525464, -122.672964]],
+  
   findMap(container, options) {
     return new this.googleMaps.Map(container, options);
   },
@@ -16,25 +18,36 @@ export default Ember.Service.extend({
     var map = new this.googleMaps.Map(container, options);
     var geocoder = new this.googleMaps.Geocoder();
     var self = this;
+    var setMarker = this.setMarker(map);
     geocoder.geocode({'address': address}, function(geoResults, status) {
       self.set('results', geoResults[0]);
       console.log(self.get('results'));
       if (status === google.maps.GeocoderStatus.OK) {
         //console.log(results);
         map.setCenter(geoResults[0].geometry.location)
-        var marker = new google.maps.Marker({
-          map: map,
-          position: geoResults[0].geometry.location
-        });
-
+        // var marker = new google.maps.Marker({
+        //   map: map,
+        //   position: geoResults[0].geometry.location,
+        //   //icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+        // });
+        setMarker;
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
-      //console.log(self.get('results'));
       self.set('lat', ((self.get('results.geometry.bounds.R.R') + self.get('results.geometry.bounds.R.j')) / 2));
       self.set('lng', ((self.get('results.geometry.bounds.j.R') + self.get('results.geometry.bounds.j.j')) / 2));
-      console.log(self.get('lat'));
-      console.log(self.get('lng'));
     });
   },
+  setMarker(map) {
+    var places = this.get('places');
+    for(var i = 0; i < places.length; i++) {
+      var place = places[i];
+
+      var marker = new google.maps.Marker({
+        map: map,
+        position: {lat: place[1], lng: place[2]},
+        icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+      });
+    }
+  }
 });
