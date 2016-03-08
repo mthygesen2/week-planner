a5764757ac8d609293f7b26bffd57b90b820ac27
Illia <ikuchko@gmail.com>
2016-03-07 22:01:42 -0800

    refactor code of yelp request

--- app/components/api-test.js
+++ app/components/api-test.js
@@ -3,36 +3,62 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   yelpApi: Ember.inject.service(),
   ajax: Ember.inject.service(),
+  yelpResult: '',
+  isShowes: false,
 
   actions: {
     sendRequest(model) {
-      var params = "Portland";
-      var date = Math.floor(Date.now()/1000);
-      var nonce = (Math.floor(Math.random() * 1e12).toString());
-      // var date = '1457409718';
-      // var nonce = 'ny14CGoipTy';
-      var signature = this.get('yelpApi').getSignature(nonce, date, params);
-      console.log(date)
-      var url = 'https://api.yelp.com/v2/search?oauth_consumer_key=s5HPEtEzcXAopt3qEA8uyg&oauth_token=kVXVaCW2aHjC_nb4LHs1xdBfrJd7fzKH&oauth_signature_method=HMAC-SHA1&oauth_signature='+signature+ '&oauth_timestamp='+date+'&oauth_nonce='+nonce+'&location='+params;
-      return Ember.$.ajax(url, {
+      // var params = "Portland";
+      // var limit = '15';
+      // var date = Math.floor(Date.now()/1000);
+      // var nonce = (Math.floor(Math.random() * 1e12).toString());
+      // var signature = this.get('yelpApi').getSignature(nonce, date, params, limit);
+      // var url = 'https://api.yelp.com/v2/search?oauth_consumer_key=s5HPEtEzcXAopt3qEA8uyg&oauth_token=kVXVaCW2aHjC_nb4LHs1xdBfrJd7fzKH&oauth_signature_method=HMAC-SHA1&oauth_signature='+signature+ '&oauth_timestamp='+date+'&oauth_nonce='+nonce+'&location='+params+'&limit='+limit;
+      // return Ember.$.ajax(url, {
+      //   dataType: 'jsonp',
+      //   jsonpCallback: 'mycallback',
+      //   cache: true
+      // }).then(function(response) {
+      //   console.log(response);
+      //   console.log(JSON.stringify(response));
+      //   return response.result;
+      // });
+      var params = {
+        location: 'Portland',
+      };
+      var urlYelp = 'https://api.yelp.com/v2/search';
+      var parameters = {
+    		oauth_consumer_key : 's5HPEtEzcXAopt3qEA8uyg',
+    		oauth_token : 'kVXVaCW2aHjC_nb4LHs1xdBfrJd7fzKH',
+    		oauth_nonce : (Math.floor(Math.random() * 1e12).toString()),
+    		// oauth_nonce : 'zrl7Ufr2JTM',
+    		// oauth_timestamp : '1457409085',
+        oauth_timestamp : Math.floor(Date.now()/1000),
+    		oauth_signature_method : 'HMAC-SHA1',
+        callback: 'mycallback',
+    	};
+      for (var attributeName in params) { parameters[attributeName] = params[attributeName]};
+      var signature = this.get('yelpApi').getSignature(urlYelp, parameters);
+      parameters.oauth_signature = signature;
+      var result;
+      Ember.$.ajax({
+        url: urlYelp,
+        data: parameters,
         dataType: 'jsonp',
         jsonpCallback: 'mycallback',
         cache: true
       }).then(function(response) {
+        // this.result = response;
         console.log(response);
+        // this.set('yelpResult', response);
+        // this.set('isShowes', true);
+        // console.log(JSON.stringify(response));
         return response.result;
       });
+      // console.log(result);
+      // console.log(searchYelp(params));
     }
   }
 });
 
-
-//
-// GET\u0026https%3A%2F%2Fapi.yelp.com%2Fv2%2Fsearch\u0026location%3DPortland%26oauth_consumer_key%3Ds5HPEtEzcXAopt3qEA8uyg%26oauth_nonce%3DJTrdAENXTOT%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1457395198%26oauth_token%3DkVXVaCW2aHjC_nb4LHs1xdBfrJd7fzKH
-//
-// GET\u0026https%3A%2F%2Fapi.yelp.com%2Fv2%2Fsearch\u0026location%3DPortland%26oauth_consumer_key%3Ds5HPEtEzcXAopt3qEA8uyg%26oauth_nonce%3D3QrImBghW3N%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1457395335%26oauth_token%3DkVXVaCW2aHjC_nb4LHs1xdBfrJd7fzKH
-//
-//
-// https://api.yelp.com/v2/search?oauth_consumer_key=s5HPEtEzcXAopt3qEA8uyg&oauth_token=kVXVaCW2aHjC_nb4LHs1xdBfrJd7fzKH&oauth_signature_method=HMAC-SHA1&oauth_signature=jQ/SnGSgmRYfZzDGk3wHYlG85dc=&oauth_timestamp=1457395508&oauth_nonce=6rVF0tWCb0o&location=Portland
-//
-// https://api.yelp.com/v2/search?oauth_consumer_key=s5HPEtEzcXAopt3qEA8uyg&oauth_token=kVXVaCW2aHjC_nb4LHs1xdBfrJd7fzKH&oauth_signature_method=HMAC-SHA1&oauth_signature=wOKQz3jxhTemel/wvYqpO+DymIY=&oauth_timestamp=1457395198&oauth_nonce=JTrdAENXTOT&location=Portland
+//http://stackoverflow.com/questions/14220321/how-do-i-return-the-response-from-an-asynchronous-call
--- app/services/yelp-api.js
+++ app/services/yelp-api.js
@@ -10,34 +10,8 @@ export default Ember.Service.extend({
   // tokenSecret: 'M9P4ZXaLGLPAC8sekCYIetzQdaQ',
 
 
-  getSignature(nonce, timestamp, location) {
-    // var yelp = new Yelp({
-    //   consumer_key: 's5HPEtEzcXAopt3qEA8uyg',
-    //   consumer_secret: 'BLtzvHCPEIe9pHqIh9OPId8jqf4',
-    //   token: 'kVXVaCW2aHjC_nb4LHs1xdBfrJd7fzKH',
-    //   token_secret: 'M9P4ZXaLGLPAC8sekCYIetzQdaQ',
-    // });
-    //
-    // yelp.search({ term: 'food', location: 'Montreal' })
-    // .then(function (data) {
-    //   console.log(data);
-    // })
-    // .catch(function (err) {
-    //   console.error(err);
-    // });
+  getSignature(url, parameters) {
     var httpMethod = 'Get';
-    var url = 'https://api.yelp.com/v2/search';
-	  var parameters = {
-  		oauth_consumer_key : 's5HPEtEzcXAopt3qEA8uyg',
-  		oauth_token : 'kVXVaCW2aHjC_nb4LHs1xdBfrJd7fzKH',
-  		oauth_nonce : nonce,
-  		// oauth_nonce : 'zrl7Ufr2JTM',
-  		// oauth_timestamp : '1457409085',
-      oauth_timestamp : timestamp,
-  		oauth_signature_method : 'HMAC-SHA1',
-      location: location,
-      callback: 'mycallback',
-  	};
     var consumerSecret = 'BLtzvHCPEIe9pHqIh9OPId8jqf4';
 	  var tokenSecret = 'M9P4ZXaLGLPAC8sekCYIetzQdaQ';
     return oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
