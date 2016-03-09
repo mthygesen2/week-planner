2a3479886920f08e3d77e8ac762474d78b10483b
NathanRomike <nathanromike@gmail.com>
2016-03-08 23:12:34 -0800

    Add function to display one question at a time.

--- app/components/modal-next-steps.js
+++ app/components/modal-next-steps.js
@@ -2,60 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   responseService: Ember.inject.service('user-responses'),
-  showQuestions: true,
-  showQuestionOne: true,
-  showQuestionTwo: false,
-  showQuestionThree: false,
-  showQuestionFour: false,
+  showQuestions: false,
+  index: 1,
 
   actions: {
+    showQuestions() {
+      this.set('showQuestions', true);
+      // this.set('nextQuestion', );
+    },
+    // var this.get('responseService.filters.' + id) = false,
     answerQuestion(response, id) {
+      var nextQuestion = id + 1;
       var answerParam = this.get('responseService.filters.' + id + '.answer');
       answerParam.addObject(response);
-      console.log(this.get('responseService.filters.' + id + '.answer'));
-      if (response === false) {
-        this.set('showQuestionTwo', true);
-        // Remove results related to date night
-      } else {
-        this.set('showQuestionFive', true);
-        // Populate results for date night
+      if (response === false) { // Remove results based on question
+        this.set('currentId', nextQuestion);
+      } else { // Populate results based on question
+        this.set('currentId', true);
       }
       this.set('showQuestionOne', false);
-    },
-
-    answerQuestionTwo(response) {
-      var serviceAnswerParam = this.get('responseService.filters.2.answer');
-      serviceAnswerParam.addObject(response);
-      if (response === false) {
-        // Remove results related to question 2 params'
-      } else {
-        // Populate tech meetups
-      }
-      this.set('showQuestionTwo', false);
-      this.set('showQuestionThree', true);
-    },
-
-    answerQuestionThree(response) {
-      var serviceAnswerParam = this.get('responseService.filters.3.answer');
-      serviceAnswerParam.addObject(response);
-      if (response === false) {
-        // Remove Group Hangouts
-      } else {
-        // Populate Group Hangouts
-      }
-      this.set('showQuestionThree', false);
-      this.set('showQuestionFour', true);
-    },
-
-    answerQuestionFour(response) {
-      var serviceAnswerParam = this.get('responseService.filters.4.answer');
-      serviceAnswerParam.addObject(response);
-      if (response === false) {
-        // Remove Disco
-      } else {
-        // Populate Disco
-      }
-      this.set('showQuestionFour', false);
     },
   }
 });
--- app/services/user-responses.js
+++ app/services/user-responses.js
@@ -3,43 +3,43 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   filters: [
     {
-      "id": 0,
+      "id": 1,
       "question": 'Date Night?',
       "answer": [],
     },
 
     {
-      "id": 1,
+      "id": 2,
       "question": 'Tech Meetup?',
       "answer": [],
     },
 
     {
-      "id": 2,
+      "id": 3,
       "question": 'Group Hangout?',
       "answer": [],
     },
 
     {
-      "id": 3,
+      "id": 4,
       "question": 'Disco?',
       "answer": [],
     },
 
     {
-      "id": 4,
+      "id": 5,
       "question": 'Drinks before dinner?',
       "answer": [],
     },
 
     {
-      "id": 5,
+      "id": 6,
       "question": 'Movie after dinner?',
       "answer": [],
     },
 
     {
-      "id": 6,
+      "id": 7,
       "question": 'New release, or second run movie?',
       "answer": [],
     },
