30fa2c3cc020409e046ee00a1f8bb5e1878c4bda
Illia <ikuchko@gmail.com>
2016-03-09 16:55:55 -0800

    create functionality of first page

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   googleMap: Ember.inject.service(),
   yelpApi: Ember.inject.service(),
-  
+  foursquareApi: Ember.inject.service(),
+
+  actions: {
+    getLocation() {
+      this.transitionTo('top-picks');
+    }
+  }
 });
