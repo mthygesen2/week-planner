a0a21a5ae8bdbad5b9ed166e7264d016ce7c2dfb
Illia <ikuchko@gmail.com>
2016-03-11 09:51:50 -0800

    fix cards size on main page in the slider

--- app/components/main-drag-area.js
+++ app/components/main-drag-area.js
@@ -5,6 +5,31 @@ export default Ember.Component.extend({
   googleMaps: Ember.inject.service(),
   selectedItems: Ember.inject.service(),
 
+  breakpoints: [
+		{
+		  'breakpoint': 1024,
+		  'settings': {
+		    'slidesToShow': 3,
+		    'slidesToScroll': 3,
+		    'infinite': true
+		  }
+		},
+		{
+		  'breakpoint': 600,
+		  'settings': {
+		    'slidesToShow': 2,
+		    'slidesToScroll': 2
+		  }
+		},
+		{
+		  'breakpoint': 480,
+		  'settings': {
+		    'slidesToShow': 1,
+		    'slidesToScroll': 1
+		  }
+		}
+	],
+
 
   actions: {
     droppedDinner(businessId) {
@@ -53,7 +78,7 @@ export default Ember.Component.extend({
       $('#spin').click(function() {
       for (var i = 1; i < 4; i ++) {
         var current = '#' + i;
-        $(current).slick('slickGoTo', Math.random() * 10);
+        $(current).slick('slickGoTo', Math.random() * 15);
       }
     });
     $('.venue__img').click(function(){
@@ -64,34 +89,35 @@ export default Ember.Component.extend({
     });
     return false;
 });
-  $('.map__footer').slick({
-  centerMode: true,
-  infinite: true,
-  speed: 500,
-  prevArrow : '<button type="button" class="slick-prev"> <</button>',
-  nextArrow : '<button type="button" class="slick-next"> ></button>',
-  swipeToSlide: true,
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 1250,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        slidesToShow: 1
-      }
+
+$('.map__footer').slick({
+centerMode: true,
+infinite: true,
+speed: 500,
+prevArrow : '<button type="button" class="slick-prev"> </button>',
+nextArrow : '<button type="button" class="slick-next"> ></button>',
+swipeToSlide: true,
+slidesToShow: 3,
+responsive: [
+  {
+    breakpoint: 1250,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      slidesToShow: 2,
     }
-  ]
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      slidesToShow: 1
+    }
+  }
+]
 });
+$( "#spin" ).trigger( "click" );
     });
   }
 });
